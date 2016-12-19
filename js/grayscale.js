88db45a4106776df932e71113221568e0e7883e1
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-12-19 22:38:49 +0000

    random tinkering

--- js/grayscale.js
+++ js/grayscale.js
@@ -24,41 +24,42 @@ $(document).ready(function(){
   }
   $('.progress-pie-chart.blue .ppc-progress-fill').css('transform','rotate('+ degBlue +'deg)');
   $('.progress-pie-chart.blue .ppc-percents span').html(percentBlue+'%');
+
+
+  // Animations
+  $window = $(window)
+  $features = $('.feature')
+  $carousels = $('.carousel')
+  $window.on('scroll', check_if_in_view);
+
+  function check_if_in_view() {
+    var windowHeight = $window.height();
+    var topOfWindow = $window.scrollTop();
+    var bottomOfWindow = (topOfWindow + windowHeight);
+
+    $features.each(function () {
+      var $this = $(this)
+      var thisHeight = $this.outerHeight();
+      var thisTop = $this.offset().top;
+      var thisBottom = (thisTop + thisHeight);
+
+      if ((thisBottom >= topOfWindow) && (thisTop <= bottomOfWindow)) {
+        $this.addClass('slideUp');
+      }
+    });
+
+    $carousels.each(function() {
+      var $element = $(this)
+      var elementHeight = $element.outerHeight();
+      var elementTop = $element.offset().top;
+      var elementBottom = (elementTop + elementHeight);
+
+      if ((elementTop <= bottomOfWindow && elementBottom >= topOfWindow)) {
+        $element.addClass('fadeIn');
+      }
+    });
+  };
 });
-
-// Animations
-$window = $(window)
-$features = $('.feature')
-$carousels = $('.carousel')
-$window.on('scroll', check_if_in_view);
-
-function check_if_in_view() {
-  var windowHeight = $window.height();
-  var topOfWindow = $window.scrollTop();
-  var bottomOfWindow = (topOfWindow + windowHeight);
-
-  $features.each(function () {
-    var $this = $(this)
-    var thisHeight = $this.outerHeight();
-    var thisTop = $this.offset().top;
-    var thisBottom = (thisTop + thisHeight);
-
-    if ((thisBottom >= topOfWindow) && (thisTop <= bottomOfWindow)) {
-      $this.addClass('slideUp');
-    }
-  });
-
-  // $carousels.each(function() {
-  //   var $element = $(this)
-  //   var elementHeight = $element.outerHeight();
-  //   var elementTop = $element.offset().top;
-  //   var elementBottom = (elementTop + elementHeight);
-
-  //   if ((elementTop <= bottomOfWindow && elementBottom >= topOfWindow)) {
-  //     $element.addClass('fadeIn');
-  //   }
-  // });
-};
 
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
