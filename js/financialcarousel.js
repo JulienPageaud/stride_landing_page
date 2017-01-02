529035b0ea2106d94348ce0f34a204b452afe98c
Christos Dimitroulas <c.dimitroulas@gmail.com>
2017-01-02 12:09:14 +0000

    carousel responsive for mobile devices

--- js/financialcarousel.js
+++ js/financialcarousel.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
-  $("#cloud9-carousel").Cloud9Carousel( {
-    autoPlay: 1,
-    bringToFront: true
-  } );
+  var mediaQuery = window.matchMedia( "(min-width: 768px)" );
 
+  if (mediaQuery.matches) {
+    $("#cloud9-carousel").Cloud9Carousel( {
+      autoPlay: 1,
+      bringToFront: true
+    } );
+  }
+  else {
+    $("#cloud9-carousel").Cloud9Carousel( {
+      xOrigin: 0,
+      yOrigin: null,
+      xRadius: 130,
+      yRadius: 60,
+      farScale: 0.1,
+      autoPlay: 1,
+      bringToFront: true
+    } );
+  }
   // var nearestEl = $('#cloud9-carousel').data('cloud9-carousel').nearestItem()['element'];
   // $('cloud9-item').removeClass('front-item').addClass('side-item');
   // $nearestEl.addClass('front-item').removeClass('side-item');
